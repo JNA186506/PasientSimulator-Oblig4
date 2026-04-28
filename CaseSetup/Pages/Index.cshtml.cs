1a51665c8be824855f8d74265741da0e862993be
587712 <587712@stud.hvl.no>
2026-04-28 12:15:19 +0200

    Updated CaseSetup

--- CaseSetup/Pages/Index.cshtml.cs
+++ CaseSetup/Pages/Index.cshtml.cs
@@ -1,13 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PasientSimulator.lib.Models;
+using PasientSimulator.lib.Services;
 
 namespace CaseSetup.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-
+        public Context context { get; set; }
+        public CaseService caseService { get; set; }
+        public List<Case> cases { get; set; }
+        public IndexModel() {
+            context = new Context();
+            caseService = new CaseService(context);
+            cases = caseService.GetAllCases();
         }
     }
 }
